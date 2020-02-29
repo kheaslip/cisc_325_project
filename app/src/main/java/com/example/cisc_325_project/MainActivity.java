f30b9d224956cc3c58d8793eec28ece9504d57a0
Kyle Heaslip <kheaslip.git@gmail.com>
2020-02-29 16:31:23 -0500

    Add array of sample friends

--- app/src/main/java/com/example/cisc_325_project/MainActivity.java
+++ app/src/main/java/com/example/cisc_325_project/MainActivity.java
@@ -6,12 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final ArrayList<Person> people = new ArrayList<Person>();
+
+        people.add(new Person("Sasha Beltran", R.drawable.profile_woman_1, "At the gym"));
+        people.add(new Person("Harmony Bennett", R.drawable.profile_woman_2, "Studying"));
+        people.add(new Person("Tori Mcbride", R.drawable.profile_woman_3, "At the dinning hall"));
+        people.add(new Person("Saniya Hodson", R.drawable.profile_woman_4, "Playing basketball"));
+
+        people.add(new Person("Colin Woodard",R.drawable.profile_man_1,"At the dinning hall"));
+        people.add(new Person("Mikhail Sanderson",R.drawable.profile_man_2,"Studying"));
+        people.add(new Person("Blessing Baldwin",R.drawable.profile_man_3,"At conference"));
+        people.add(new Person("Zacharias Phelps",R.drawable.profile_man_4,"Playing Go"));
+
     }
 
 
