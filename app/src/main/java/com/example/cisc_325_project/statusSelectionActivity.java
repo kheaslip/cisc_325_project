0a81211ef4e09580225c33a4a070208c1873b278
Kyle Heaslip <kheaslip.git@gmail.com>
2020-03-01 16:09:11 -0500

    Add onclick method to copy the preset status into the selection bar

--- app/src/main/java/com/example/cisc_325_project/statusSelectionActivity.java
+++ app/src/main/java/com/example/cisc_325_project/statusSelectionActivity.java
@@ -3,6 +3,8 @@ package com.example.cisc_325_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class statusSelectionActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class statusSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_status_selection);
+    }
+
+
+    public void updateStatus(View view) {
+        ((TextView) findViewById(R.id.change_status)).setText(((TextView) view).getText());
     }
 }
