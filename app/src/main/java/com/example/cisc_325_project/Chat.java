6fbec32dfd2ce0c7a5aa7dc277b0d80b93b9bba9
Kyle Heaslip <kheaslip.git@gmail.com>
2020-03-01 15:01:03 -0500

    Add basic ability to 'send' messages in the chat screen

--- app/src/main/java/com/example/cisc_325_project/Chat.java
+++ app/src/main/java/com/example/cisc_325_project/Chat.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class Chat extends AppCompatActivity {
@@ -23,5 +25,22 @@ public class Chat extends AppCompatActivity {
         ((TextView)findViewById(R.id.chat_receiver_name)).setText(name);
         ((TextView)findViewById(R.id.chat_receiver_status)).setText(status);
         ((ImageView)findViewById(R.id.chat_receiver_image)).setImageResource(image);
+    }
+
+    public void addMessageToChatLog(View view) {
+        TextView chatBar = (TextView) findViewById(R.id.chat_text_bar);
+        CharSequence message = chatBar.getText();
+        chatBar.setText("");
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.chat_log);
+        TextView textView = new TextView(this);
+
+        textView.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        ));
+        textView.setText(message);
+        linearLayout.addView(textView);
+
     }
 }
