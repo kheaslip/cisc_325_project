bdc44b975404fa53fe732da01d65356664c77e22
Kyle Heaslip <kheaslip.git@gmail.com>
2020-03-01 11:26:47 -0500

    Made Person object serializable
    Need to pass an ArrayList of Person objects between the event list screen
    and the event details screen; this requires that the Person object be
    serializable for an ArrayList to be put within an Intent.

--- app/src/main/java/com/example/cisc_325_project/Person.java
+++ app/src/main/java/com/example/cisc_325_project/Person.java
@@ -1,6 +1,8 @@
 package com.example.cisc_325_project;
 
-public class Person {
+import java.io.Serializable;
+
+public class Person implements Serializable {
 
     private String mName;
     private String mStatus;
