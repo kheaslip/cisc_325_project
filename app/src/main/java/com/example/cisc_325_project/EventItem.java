509104bc1623e496a145d72417697b34bc6cd50b
Kyle Heaslip <kheaslip.git@gmail.com>
2020-03-01 11:11:23 -0500

    Add start date, end date, and location to EventItem class

--- app/src/main/java/com/example/cisc_325_project/EventItem.java
+++ app/src/main/java/com/example/cisc_325_project/EventItem.java
@@ -1,17 +1,24 @@
 package com.example.cisc_325_project;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class EventItem {
 
     private String mName;
+    private Date mStartDate;
+    private Date mEndDate;
+    private String mLocation;
     private String mDetails;
     private int mImageResource;
     private ArrayList<Person> mAttendees;
 
-
-    public EventItem(String name, String details, int imageResource, ArrayList<Person> attendees) {
+    public EventItem(String name, Date start, Date end, String location, String details,
+                     int imageResource, ArrayList<Person> attendees) {
         this.mName = name;
+        this.mStartDate = start;
+        this.mEndDate = end;
+        this.mLocation = location;
         this.mDetails = details;
         this.mImageResource = imageResource;
         this.mAttendees = attendees;
@@ -19,6 +26,18 @@ public class EventItem {
 
     public String getmName() {
         return mName;
+    }
+
+    public Date getmStartDate() {
+        return new Date(mStartDate.toString());
+    }
+
+    public Date getmEndDate() {
+        return new Date(mEndDate.toString());
+    }
+
+    public String getmLocation() {
+        return mLocation;
     }
 
     public String getmDetails() {
