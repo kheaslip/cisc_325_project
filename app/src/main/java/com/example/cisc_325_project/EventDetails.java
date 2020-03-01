2734e221fd09b05cc2441c06a0c34e01bdeadcf6
Kyle Heaslip <kheaslip.git@gmail.com>
2020-03-01 11:27:46 -0500

    Add code to display the event details sent to the event details screen

--- app/src/main/java/com/example/cisc_325_project/EventDetails.java
+++ app/src/main/java/com/example/cisc_325_project/EventDetails.java
@@ -2,7 +2,13 @@ package com.example.cisc_325_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class EventDetails extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class EventDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        Intent intent =getIntent();
+
+        String name = intent.getStringExtra("name");
+        Date startDate = (Date) intent.getSerializableExtra("start");
+        Date endDate = (Date) intent.getSerializableExtra("end");
+        String location = intent.getStringExtra("location");;
+        String details = intent.getStringExtra("details");;
+        int imageResource = intent.getIntExtra("image", 0);
+        ArrayList<Person> attendees = (ArrayList<Person>) intent.getSerializableExtra("attendees");
+
+        ((TextView)findViewById(R.id.activity_event_details_name)).setText(name);
+        ((TextView)findViewById(R.id.activity_event_details_date)).setText(startDate.toString()+ " - " + endDate.toString());
+        ((TextView)findViewById(R.id.activity_event_details_location)).setText(location);
+        ((TextView)findViewById(R.id.activity_event_details_details)).setText(details);
+        ((TextView)findViewById(R.id.activity_event_details_time)).setText("N/A");
+        ((ImageView)findViewById(R.id.activity_event_details_image)).setImageResource(imageResource);
+
     }
 }
