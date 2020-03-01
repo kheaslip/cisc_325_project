c352f6ebaa649177cf2d141d8817ba533dbb7799
Kyle Heaslip <kheaslip.git@gmail.com>
2020-03-01 12:14:34 -0500

    Add stub onClick method to 'Create' button in the new event screen

--- app/src/main/java/com/example/cisc_325_project/CreateEvent.java
+++ app/src/main/java/com/example/cisc_325_project/CreateEvent.java
@@ -3,6 +3,8 @@ package com.example.cisc_325_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class CreateEvent extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class CreateEvent extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+    }
+
+    public void createEventButton(View view) {
+        (Toast.makeText(this, "Not able to create events until later in development", Toast.LENGTH_LONG)).show();
     }
 }
