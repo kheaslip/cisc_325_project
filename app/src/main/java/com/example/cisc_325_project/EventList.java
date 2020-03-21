7f069d5cc4b41ed71d22568ff749471ccb1ed4d6
Kyle Heaslip <kheaslip.git@gmail.com>
2020-03-21 16:50:27 -0400

    Convert the events activity into a fragment for the home screen viewPager

--- app/src/main/java/com/example/cisc_325_project/EventList.java
+++ app/src/main/java/com/example/cisc_325_project/EventList.java
@@ -1,11 +1,17 @@
 package com.example.cisc_325_project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MotionEvent;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
@@ -14,115 +20,120 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 
-public class EventList extends AppCompatActivity {
+import static com.example.cisc_325_project.R.id.activity_events_list;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EventList extends Fragment {
+
+    public EventList() {
+        // Required empty public constructor
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_events, container, false);
+    }
 
-        final ArrayList<EventItem> events = new ArrayList<EventItem>();
-        final ArrayList<Person> people = new ArrayList<Person>();
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-        people.add(new Person("Sasha Beltran", R.drawable.profile_woman_1, "At the gym"));
-        people.add(new Person("Harmony Bennett", R.drawable.profile_woman_2, "Studying"));
-        people.add(new Person("Tori Mcbride", R.drawable.profile_woman_3, "At the dinning hall"));
-        people.add(new Person("Saniya Hodson", R.drawable.profile_woman_4, "Playing basketball"));
+        // setup list of events
+        {
+            final ArrayList<EventItem> events = new ArrayList<EventItem>();
+            final ArrayList<Person> people = new ArrayList<Person>();
 
-        people.add(new Person("Colin Woodard",R.drawable.profile_man_1,"At the dinning hall"));
-        people.add(new Person("Mikhail Sanderson",R.drawable.profile_man_2,"Studying"));
-        people.add(new Person("Blessing Baldwin",R.drawable.profile_man_3,"At conference"));
-        people.add(new Person("Zacharias Phelps",R.drawable.profile_man_4,"Playing Go"));
+            people.add(new Person("Sasha Beltran", R.drawable.profile_woman_1, "At the gym"));
+            people.add(new Person("Harmony Bennett", R.drawable.profile_woman_2, "Studying"));
+            people.add(new Person("Tori Mcbride", R.drawable.profile_woman_3, "At the dinning hall"));
+            people.add(new Person("Saniya Hodson", R.drawable.profile_woman_4, "Playing basketball"));
 
-        events.add(new EventItem("Canada Day Firework Show",
-                new Date(2020, 6, 1, 21,0,0),
-                new Date(2020, 6, 1, 22,0,0),
-                "Kingston",
-                "The annual fireworks display for Canada day",
-                R.drawable.event_image_fireworks,
-                new ArrayList<>(Arrays.asList(
-                        people.get(0), people.get(1), people.get(2), people.get(3)
-                ))));
+            people.add(new Person("Colin Woodard",R.drawable.profile_man_1,"At the dinning hall"));
+            people.add(new Person("Mikhail Sanderson",R.drawable.profile_man_2,"Studying"));
+            people.add(new Person("Blessing Baldwin",R.drawable.profile_man_3,"At conference"));
+            people.add(new Person("Zacharias Phelps",R.drawable.profile_man_4,"Playing Go"));
 
-        events.add(new EventItem("Local band tour",
-                new Date(2020, 3, 21, 20,0,0),
-                new Date(2020, 3, 21, 23,0,0),
-                "Kingston",
-                "Several local bands playing this weekend",
-                R.drawable.event_image_concert,
-                new ArrayList<>(Arrays.asList(
-                        people.get(4), people.get(7), people.get(2), people.get(0)
-                ))));
+            events.add(new EventItem("Canada Day Firework Show",
+                    new Date(2020, 6, 1, 21,0,0),
+                    new Date(2020, 6, 1, 22,0,0),
+                    "Kingston",
+                    "The annual fireworks display for Canada day",
+                    R.drawable.event_image_fireworks,
+                    new ArrayList<>(Arrays.asList(
+                            people.get(0), people.get(1), people.get(2), people.get(3)
+                    ))));
 
-        events.add(new EventItem("Orientation Week",
-                new Date(2020, 9, 1, 8,0,0),
-                new Date(2020, 12, 1, 23,0,0),
-                "Kingston",
-                "Join your new classmates as we play games.",
-                R.drawable.event_image_crowd_1,
-                new ArrayList<>(Arrays.asList(
-                        people.get(4), people.get(2), people.get(5), people.get(1)
-                ))));
+            events.add(new EventItem("Local band tour",
+                    new Date(2020, 3, 21, 20,0,0),
+                    new Date(2020, 3, 21, 23,0,0),
+                    "Kingston",
+                    "Several local bands playing this weekend",
+                    R.drawable.event_image_concert,
+                    new ArrayList<>(Arrays.asList(
+                            people.get(4), people.get(7), people.get(2), people.get(0)
+                    ))));
 
-        events.add(new EventItem("Karaoke",
-                new Date(2020, 3, 14, 20,0,0),
-                new Date(2020, 3, 14, 23,0,0),
-                "Kingston",
-                "Weekly karaoke night where you and your friends can battle-off.",
-                R.drawable.event_image_karaoke,
-                new ArrayList<>(Arrays.asList(
-                        people.get(4), people.get(7), people.get(2), people.get(0), people.get(5),
-                        people.get(1), people.get(6), people.get(3)
-                ))));
+            events.add(new EventItem("Orientation Week",
+                    new Date(2020, 9, 1, 8,0,0),
+                    new Date(2020, 12, 1, 23,0,0),
+                    "Kingston",
+                    "Join your new classmates as we play games.",
+                    R.drawable.event_image_crowd_1,
+                    new ArrayList<>(Arrays.asList(
+                            people.get(4), people.get(2), people.get(5), people.get(1)
+                    ))));
+
+            events.add(new EventItem("Karaoke",
+                    new Date(2020, 3, 14, 20,0,0),
+                    new Date(2020, 3, 14, 23,0,0),
+                    "Kingston",
+                    "Weekly karaoke night where you and your friends can battle-off.",
+                    R.drawable.event_image_karaoke,
+                    new ArrayList<>(Arrays.asList(
+                            people.get(4), people.get(7), people.get(2), people.get(0), people.get(5),
+                            people.get(1), people.get(6), people.get(3)
+                    ))));
 
 
 
-        EventAdapter eventAdapter = new EventAdapter(this.getBaseContext(), events);
-        ListView listView = (ListView) findViewById(R.id.activity_events_list);
+            EventAdapter eventAdapter = new EventAdapter(this.getContext(), events);
+            ListView listView =  getView().findViewById(activity_events_list);
 
-        listView.setAdapter(eventAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            listView.setAdapter(eventAdapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Intent intent = new Intent(getActivity().getApplicationContext(), EventDetails.class);
+                    EventItem eventItem = events.get(position);
+
+                    intent.putExtra("name", eventItem.getmName());
+                    intent.putExtra("start", eventItem.getmStartDate());
+                    intent.putExtra("end", eventItem.getmEndDate());
+                    intent.putExtra("location", eventItem.getmLocation());
+                    intent.putExtra("details", eventItem.getmDetails());
+                    intent.putExtra("image", eventItem.getmImageResource());
+                    intent.putExtra("attendees", eventItem.getmAttendees());
+
+                    startActivity(intent);
+
+                }
+            });
+        }
+
+        // give the 'create new event' button an onClick listener
+        Button button = (Button) getView().findViewById(R.id.button_create_event);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(), EventDetails.class);
-                EventItem eventItem = events.get(position);
-
-                intent.putExtra("name", eventItem.getmName());
-                intent.putExtra("start", eventItem.getmStartDate());
-                intent.putExtra("end", eventItem.getmEndDate());
-                intent.putExtra("location", eventItem.getmLocation());
-                intent.putExtra("details", eventItem.getmDetails());
-                intent.putExtra("image", eventItem.getmImageResource());
-                intent.putExtra("attendees", eventItem.getmAttendees());
-
-                startActivity(intent);
-
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), CreateEvent.class));
             }
         });
-
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (Home.hasStatus()) {
-            ((Button)findViewById(R.id.status_button)).setText(Home.getmStatus());
-        }
-    }
 
-    public void loadEventCreationScreen(View view) {
-        startActivity(new Intent(EventList.this, CreateEvent.class));
-    }
-
-    public void loadStatusScreen(View view) {
-        startActivity(new Intent(EventList.this, statusSelectionActivity.class));
-    }
-
-    public void loadHomeScreen(View view) {
-        startActivity(new Intent(EventList.this, Home.class));
-    }
-
-    public void loadProfileScreen(View view) {
-        startActivity(new Intent(EventList.this, Profile.class));
-    }
 }
