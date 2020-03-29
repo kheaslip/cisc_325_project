6fc364b790ce31b718e7b3fed4c5f22ad5270ba2
Kyle Heaslip <kheaslip.git@gmail.com>
2020-03-29 10:04:27 -0400

    Add click functionality to the custom map info window

--- app/src/main/java/com/example/cisc_325_project/MapsActivity.java
+++ app/src/main/java/com/example/cisc_325_project/MapsActivity.java
@@ -1,15 +1,38 @@
 package com.example.cisc_325_project;
 
-import androidx.fragment.app.FragmentActivity;
+/**
+* Rough tutorial for how to create map with custom marker info windows with clickable elements
+* provided by <a href="https://stackoverflow.com/users/1970966/chose007">chose007</a> and
+* <a href="https://stackoverflow.com/users/4613320/maddy">Maddy</a> in this
+* <a href="https://stackoverflow.com/a/15040761/4696389">StackOverFlow page</a>
+*
+* Files based on this tutorial include:
+* <ul>
+*   <li>MapsActivity.java</li>
+*   <li>MapWrapperLayout.java</li>
+*   <li>OnInfoWindowElemTouchListener.java</li>
+*   <li>activity_maps.xml</li>
+*   <li>maps_info_window_person.xml</li>
+* </ul>
+*
+* */
 
-import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
@@ -24,85 +47,128 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private Context mContext;
 
-    private final ArrayList<Person> people = new ArrayList<Person>();
+    private ViewGroup mInfoWindow;
+    private ImageView mInfoImage;
+    private TextView mInfoTitle;
+    private TextView mInfoSnippet;
+    private Button mInfoButton;
+    private OnInfoWindowElemTouchListener mInfoButtonListener;
+
+    private final ArrayList<Person> mPeople = new ArrayList<Person>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        people.add(new Person("Sasha Beltran", R.drawable.profile_woman_1, "At the gym",44.229444, -76.494350));
-        people.add(new Person("Harmony Bennett", R.drawable.profile_woman_2, "Studying",44.225361, -76.498782));
-        people.add(new Person("Tori Mcbride", R.drawable.profile_woman_3, "At the dinning hall", 44.224681, -76.496035));
-        people.add(new Person("Saniya Hodson", R.drawable.profile_woman_4, "Playing basketball", 44.229316, -76.494139));
+        // the people on the map
+        {
+            mPeople.add(new Person("Sasha Beltran", R.drawable.profile_woman_1, "At the gym",44.229444, -76.494350));
+            mPeople.add(new Person("Harmony Bennett", R.drawable.profile_woman_2, "Studying",44.225361, -76.498782));
+            mPeople.add(new Person("Tori Mcbride", R.drawable.profile_woman_3, "At the dinning hall", 44.224681, -76.496035));
+            mPeople.add(new Person("Saniya Hodson", R.drawable.profile_woman_4, "Playing basketball", 44.229316, -76.494139));
 
-        people.add(new Person("Colin Woodard",R.drawable.profile_man_1,"At the dinning hall",44.224096, -76.500638));
-        people.add(new Person("Mikhail Sanderson",R.drawable.profile_man_2,"Studying",44.227311, -76.495121));
-        people.add(new Person("Blessing Baldwin",R.drawable.profile_man_3,"At conference",44.218255, -76.517759));
-        people.add(new Person("Zacharias Phelps",R.drawable.profile_man_4,"Playing Go",44.194878, -76.438305));
-
-
+            mPeople.add(new Person("Colin Woodard",R.drawable.profile_man_1,"At the dinning hall",44.224096, -76.500638));
+            mPeople.add(new Person("Mikhail Sanderson",R.drawable.profile_man_2,"Studying",44.227311, -76.495121));
+            mPeople.add(new Person("Blessing Baldwin",R.drawable.profile_man_3,"At conference",44.218255, -76.517759));
+            mPeople.add(new Person("Zacharias Phelps",R.drawable.profile_man_4,"Playing Go",44.194878, -76.438305));
+        }
 
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(final GoogleMap googleMap) {
 
-        final GoogleMap mGoogleMap = googleMap;
+        mMap = googleMap;
         mContext = this;
 
-        googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+        final MapWrapperLayout mapWrapperLayout = (MapWrapperLayout) findViewById(R.id.map_layout);
+
+        mapWrapperLayout.init(googleMap, getPixelsFromDp(this, 39 + 20));
+
+        // get the xml elements of the infoWindow
+        this.mInfoWindow = (ViewGroup)getLayoutInflater().inflate(R.layout.maps_info_window_person, null);
+        this.mInfoTitle = (TextView) mInfoWindow.findViewById(R.id.info_window_person_name);
+        this.mInfoSnippet = (TextView) mInfoWindow.findViewById(R.id.info_window_person_status);
+        this.mInfoButton = (Button) mInfoWindow.findViewById(R.id.info_window_button_chat);
+        this.mInfoImage = (ImageView) mInfoWindow.findViewById(R.id.info_window_person_image);
+
+        // set OnTouchListener that handles the checking for what was pressed
+        this.mInfoButtonListener = new OnInfoWindowElemTouchListener(mInfoButton,
+                getResources().getDrawable(R.drawable.common_google_signin_btn_icon_light_normal),
+                getResources().getDrawable(R.drawable.common_google_signin_btn_icon_light_focused))
+        {
             @Override
-            public void onMapLoaded() {
+            protected void onClickConfirmed(View v, Marker marker) {
 
-                mMap = mGoogleMap;
+                Toast.makeText(MapsActivity.this, marker.getTitle() + "'s button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        };
+        this.mInfoButton.setOnTouchListener(mInfoButtonListener);
 
-                // units dp to px
-                int padding = (int) (16 * Resources.getSystem().getDisplayMetrics().density);
-                double lat, lng;
-                Marker marker;
-                LatLngBounds.Builder builder = new LatLngBounds.Builder();
 
-                // for each person, given them their own pin on the map
-                for (Person p : people) {
-                    lat = p.getMlocationLat();
-                    lng = p.getMlocationLng();
-                    LatLng pos = new LatLng(lat, lng);
+        // define how the infoWindow is populated
+        googleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
 
-                    builder.include(pos);
-                    marker = mMap.addMarker(new MarkerOptions()
-                        .position(pos)
-                        .icon(BitmapDescriptorFactory.defaultMarker(new Random().nextInt(360)))
+            @Override
+            public View getInfoContents(Marker marker) {
 
-                    );
+                InfoWindowDataPerson personData = (InfoWindowDataPerson) marker.getTag();
 
-                    mGoogleMap.setInfoWindowAdapter(
-                            new CustomGoogleMapInfoWindowPerson(mContext));
+                // Setting up the infoWindow with current's marker info
+                mInfoTitle.setText(personData.getmName());
+                mInfoSnippet.setText(personData.getmStatus());
+                mInfoButtonListener.setMarker(marker);
+                mInfoImage.setImageResource(personData.getmImageResourceID());
 
-                    marker.setTag(new InfoWindowDataPerson(
-                            p.getmResourceImage(),p.getmName(),p.getmStatus())
-                    );
+                // set the current marker and infoWindow references to the MapWrapperLayout
+                mapWrapperLayout.setMarkerWithInfoWindow(marker, mInfoWindow);
+                return mInfoWindow;
 
-                }
-                LatLngBounds bounds = builder.build();
-                mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds,padding)); // 16dp padding
             }
         });
 
+        // units dp to px
+        int padding = (int) (16 * Resources.getSystem().getDisplayMetrics().density);
+        double lat, lng;
+        Marker marker;
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+        // for each person, given them their own pin on the map
+        for (Person p : mPeople) {
+            lat = p.getMlocationLat();
+            lng = p.getMlocationLng();
+            LatLng pos = new LatLng(lat, lng);
+
+            builder.include(pos);
+            marker = googleMap.addMarker(new MarkerOptions()
+                    .position(pos)
+                    .icon(BitmapDescriptorFactory.defaultMarker(new Random().nextInt(360)))
+
+            );
+
+            marker.setTag(new InfoWindowDataPerson(
+                    p.getmResourceImage(),p.getmName(),p.getmStatus())
+            );
+
+        }
+        LatLngBounds bounds = builder.build();
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds,
+                getResources().getDisplayMetrics().widthPixels,
+                getResources().getDisplayMetrics().heightPixels, padding));
+
+    }
+
+    public static int getPixelsFromDp(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int)(dp * scale);
     }
 }
---
+++ app/src/main/java/com/example/cisc_325_project/OnInfoWindowElemTouchListener.java
@@ -0,0 +1,110 @@
+package com.example.cisc_325_project;
+
+/**
+ * Rough tutorial for how to create map with custom marker info windows with clickable elements
+ * provided by <a href="https://stackoverflow.com/users/1970966/chose007">chose007</a> and
+ * <a href="https://stackoverflow.com/users/4613320/maddy">Maddy</a> in this
+ * <a href="https://stackoverflow.com/a/15040761/4696389">StackOverFlow page</a>
+ *
+ * Files based on this tutorial include:
+ * <ul>
+ *   <li>MapsActivity.java</li>
+ *   <li>MapWrapperLayout.java</li>
+ *   <li>OnInfoWindowElemTouchListener.java</li>
+ *   <li>activity_maps.xml</li>
+ *   <li>maps_info_window_person.xml</li>
+ * </ul>
+ *
+ * */
+
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.google.android.gms.maps.model.Marker;
+
+/**
+ * Used to determine what was clicked on the infoWindow on the google map, and handle the clicking
+ */
+public abstract class OnInfoWindowElemTouchListener implements View.OnTouchListener {
+
+    private final View mView;
+    private final Drawable mDrawableNormal;
+    private final Drawable mDrawablePressed;
+    private final Handler mHandler = new Handler();
+
+    private Marker mMarker;
+    private boolean mPressed = false;
+
+    public OnInfoWindowElemTouchListener(View view, Drawable drawableNormal, Drawable drawablePressed) {
+        this.mView = view;
+        this.mDrawableNormal = drawableNormal;
+        this.mDrawablePressed = drawablePressed;
+    }
+
+    public void setMarker(Marker marker) {
+        this.mMarker = marker;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        // make sure event is on the screen
+        if (0 <= event.getX() && event.getX() <= mView.getWidth() &&
+            0 <= event.getY() && event.getY() <= mView.getHeight())
+        {
+            switch (event.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    startPress(); break;
+
+                case MotionEvent.ACTION_UP:
+                    mHandler.postDelayed(confirmClickRunable,150); break;
+
+                case MotionEvent.ACTION_CANCEL:
+                    endPress(); break;
+
+                default: break;
+            }
+        } else {
+            endPress();
+        }
+        return false;
+    }
+
+    private void startPress() {
+        if (!mPressed) {
+            mPressed = true;
+            mHandler.removeCallbacks(confirmClickRunable);
+            mView.setBackground(mDrawablePressed);
+            if (mMarker != null) {
+                mMarker.showInfoWindow();
+            }
+        }
+    }
+
+    private boolean endPress() {
+        if (mPressed) {
+            this.mPressed = false;
+            mHandler.removeCallbacks(confirmClickRunable);
+            mView.setBackground(mDrawableNormal);
+            if (mMarker != null) {
+                mMarker.showInfoWindow();
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private final Runnable confirmClickRunable = new Runnable() {
+        @Override
+        public void run() {
+            if (endPress()) {
+                onClickConfirmed(mView, mMarker);
+            }
+        }
+    };
+
+    protected abstract void onClickConfirmed(View v, Marker marker);
+}
